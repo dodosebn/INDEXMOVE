7f30b4f17710a7578bc469602a63b5a0063dcc25
dodosebn <orjidominion32@gmail.com>
2023-08-02 04:43:23 +0400

    in the sequence

---
+++ newsletter-sign-up-with-success-message-main/index.js
@@ -0,0 +1,8 @@
+// Get the button element
+let button = document.getElementById("subscribe");
+
+// Add a click event listener to the button
+button.addEventListener("click", function() {
+  // Display an alert message
+  alert("Button clicked!");
+});
